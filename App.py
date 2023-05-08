70835e4e29a245d644872982e2f967dbce6fd95e
Szymon-Winiecki <Szymon.win4@gmail.com>
2023-05-08 22:24:02 +0200

    basic blood vessels detection

--- App.py
+++ App.py
@@ -1,6 +1,6 @@
 from tkinter import Tk, Label, Scale, Button, Text, Checkbutton, messagebox, Entry, StringVar, filedialog
 
-from PIL import ImageTk, Image, ImageDraw, ImageOps
+from PIL import ImageTk, Image, ImageDraw, ImageOps, ImageFilter, ImageEnhance
 import numpy as np
 
 import skimage as sk
@@ -11,34 +11,23 @@ class App:
 
     def __init__(self):
 
-
+        
         #display settings
-        self.maxImageDisplayWidth = 300
-        self.maxImageDisplayHeight = 400
+        self.windowWidth = 2500
+        self.windowHeight = 1300
+        self.maxImageDisplayWidth = 600
+        self.maxImageDisplayHeight = 600
 
 
         # window
         self.window = Tk()
         self.window.title('blood vessels segmentation')
-        self.window.geometry('1200x500')
+        self.window.geometry('{width}x{height}'.format(width=self.windowWidth, height=self.windowHeight))
 
-        # place for original image 
-        self.baseImage = Image.open('images/01_dr.JPG')
-        self.img = ImageTk.PhotoImage(self.resizeToFitLimits(self.baseImage))
-        self.imgLabel = Label(self.window, image=self.img)
-        self.imgLabel.grid(column=0, row=0, columnspan=2)
-
-        # place for filtered image
-        self.filtered = Image.open('blank.png')
-        self.filImg = ImageTk.PhotoImage(self.resizeToFitLimits(self.filtered))
-        self.filImgLabel = Label(self.window, image=self.filImg)
-        self.filImgLabel.grid(column=2, row=0, columnspan=2)
-
-        # place for
-        self.compareImage = Image.open('blank.png')
-        self.comImg = ImageTk.PhotoImage(self.resizeToFitLimits(self.compareImage))
-        self.comImgLabel = Label(self.window, image=self.comImg)
-        self.comImgLabel.grid(column=4, row=0, columnspan=2)
+        # images
+        self.images = []
+        self.imagePIs = []
+        self.imageLabels = []
 
 
         # computation status
@@ -56,36 +45,50 @@ class App:
 
 
         # layout
-        startButton.grid(column=3,row=5)
+        startButton.grid(column=1,row=5)
 
 
-        self.statusLabel.grid(column=4, row=2)
+        self.statusLabel.grid(column=4, row=4)
 
-        selectFileTitleLabel.grid(column=0, row = 1, sticky='W')
-        selectFileButton.grid(column=1, row = 2)
-        self.selectedFileNameLabel.grid(column=2, row=2)
+        selectFileTitleLabel.grid(column=0, row = 4, sticky='W')
+        selectFileButton.grid(column=1, row = 4)
+        self.selectedFileNameLabel.grid(column=2, row=4)
 
+        self.setImage(Image.open('images/01_dr.JPG'), 0)
 
         self.window.mainloop()
 
-    # returns image resized to fit in limits set in maxImageDisplayWidth and maxImageDisplayHeight variables
-    def resizeToFitLimits(self, image):
+    def resizeProportionally(self, image, maxWidth, maxheight):
         if image.size[0] > image.size[1]:
-            newShape = (self.maxImageDisplayHeight, int(self.maxImageDisplayHeight * (image.size[1] / image.size[0])) + 1)
+            newShape = (maxheight, int(maxheight * (image.size[1] / image.size[0])) + 1)
         else:
-            newShape = (int(self.maxImageDisplayWidth * (image.size[0] / image.size[1])) + 1, self.maxImageDisplayWidth)
+            newShape = (int(maxWidth * (image.size[0] / image.size[1])) + 1, maxWidth)
 
         return image.resize(newShape, resample=Image.BICUBIC)
 
-    # display original image
-    def setImage(self, image):
-        self.img = ImageTk.PhotoImage(self.resizeToFitLimits(image))
-        self.imgLabel.config(image=self.img)
+    # returns image resized to fit in limits set in maxImageDisplayWidth and maxImageDisplayHeight variables
+    def resizeToFitLimits(self, image):
+        return self.resizeProportionally(image, self.maxImageDisplayWidth, self.maxImageDisplayHeight)
 
-    # display filtered image
-    def setFilteredImage(self, image):
-        self.filImg = ImageTk.PhotoImage(self.resizeToFitLimits(image))
-        self.filImgLabel.config(image=self.filImg)
+    # wyświetla obrazek na podanej pozycji (lub na nowej, jeżeli nie podano)
+    def setImage(self, image, index = -1):
+            if index == -1 or index >= len(self.images):
+                index = len(self.images)
+                self.images.append(image)
+                self.imagePIs.append(ImageTk.PhotoImage(self.resizeToFitLimits(self.images[index])))
+                self.imageLabels.append(Label(self.window, image=self.imagePIs[index]))
+                row = index // (self.windowWidth // self.maxImageDisplayWidth)
+                col = index % ((self.windowWidth // self.maxImageDisplayWidth))
+                self.imageLabels[index].grid(column=col, row=row)
+            else:
+                self.images[index] = image
+                self.imagePIs[index] = ImageTk.PhotoImage(self.resizeToFitLimits(self.images[index]))
+                self.imageLabels[index].configure(image=self.imagePIs[index])
+    
+    def getImage(self, index):
+        if index >= len(self.images):
+            return Image.open('blank.png')
+        return self.images[index]
 
     
 
@@ -97,77 +100,31 @@ class App:
             self.inputImageFile = filename
             self.selectedFileNameLabel.config(text=filename[filename.rindex('/')+1:])
 
-
-            self.baseImage = Image.open(filename)
-            self.setImage(self.baseImage)
-
-
-
-    # funkcja w której filtrujemy obrazek przed wyznaczaniem naczyń
-    def preprocess(self, image):
-
-        # usuwanie kanału czerwonego (bo tylko takie mamy sprawdzac obrazki?)
-        for i in range(len(image)):
-            for j in range(len(image[0])):
-                image[i][j][0] = 0
-        image = Image.fromarray(image, mode='RGB')
-
-        # obrazek w skali szarości
-        image = np.array(image.convert('L'))
-
-        # wyostrzenie filtrem Gaussa - chyba trzeba cos lepszego
-        blurred = sk.filters.gaussian(image, sigma=2)
-        image = image - blurred
-
-        #jakis kontrast todo
-
-        return image
-
-    def findEdges(self, image):
-        filtered = sk.filters.hessian(image) # nie musi byc koniecznie ten filtr
-        return filtered
-
-    # funckja w której filtrujemy obrazek po znalezieniu naczyń
-    def postprocess(self, image):
-
-        # znajdowanie optymalnego progu(do rozrozniania tła) czy to trzeba samemu implementowac?
-        threshold = sk.filters.threshold_otsu(image)
-
-        # zamiana image na tablice boolów (True jezeli wartosc > threshold)
-        image = image > threshold
-
-        # usunięcie małych obszarów
-        image = sk.morphology.remove_small_objects(image, min_size=100)
-
-
-        # dylatacja o promieniu 2
-        image = sk.morphology.dilation(image, sk.morphology.disk(2))
-
-        # nadal zwracamy tablice z bool
-        return image
+            self.setImage(Image.open(filename), 0)
 
 
     def detectVessels(self):
-        image = np.array(self.baseImage)
 
-        image = self.preprocess(image)
-        image = self.findEdges(image)
-        image = self.postprocess(image)
+        # używamy tylko kanału M (tak mi działa lepiej niż jakieś kanały z RGB) 
+        resimage = self.getImage(0).convert('CMYK').getchannel('M')
 
-        # jak sie zapisuje matplotlibem to czasem obraz inaczej wyglada ;-;
-        #resimage = self.saveResult(image)
+        # ze względu na użyte filtru działa tylko dla małych obrazków, trzeba to będzie potem zmienić
+        resimage = self.resizeProportionally(resimage, 600, 600)
 
-        resimage = Image.fromarray(image)
+        # podbijamy kontrast
+        resimage = ImageEnhance.Contrast(resimage).enhance(1.8)
 
-        self.setFilteredImage(resimage)
+        # znajdujemy krawędzie
+        resimage = resimage.filter(ImageFilter.FIND_EDGES)
 
-    # do testow, funckja zapisujaca przefiltrowany obraz uzywajac matplotlib
-    def saveResult(self, image):
-        import matplotlib.pyplot as plt
-        # convert to grayscale using matplotlib
-        plt.imshow(image.squeeze(), cmap='gray')
-        plt.axis('off')
-        plt.savefig('output.png', bbox_inches='tight', pad_inches=0)
-        plt.close()
+        # rozmycie
+        resimage = resimage.filter(ImageFilter.BoxBlur(1))
 
-        return Image.open('output.png')
+        # progowe mapowanie na obraz czarno-biały
+        resimage = resimage.point( lambda p: 255 if p > 20 else 0 )
+
+        # wyświetlenie wyniku
+        self.setImage(resimage, 1)
+
+        # zapis do pliku
+        resimage.save("output.png")
